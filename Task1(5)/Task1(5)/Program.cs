5b82f8a68d59ac0b1713641ec1a5391005235195
Arthur N <ipz233_nad@student.ztu.edu.ua>
2025-01-23 02:13:25 +0200

    Підрахунок чисел

---
+++ Task1(5)/Task1(5)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SumCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введіть перше число:");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("Введіть друге число:");
+            string input2 = Console.ReadLine();
+
+            try
+            {
+                double number1 = Convert.ToDouble(input1);
+                double number2 = Convert.ToDouble(input2);
+
+                double sum = number1 + number2;
+
+                Console.WriteLine($"Сума {number1} і {number2} дорівнює {sum}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Будь ласка, введіть коректні числові значення.");
+            }
+
+            Console.WriteLine("Натисніть будь-яку клавішу, щоб закрити програму.");
+            Console.ReadKey();
+        }
+    }
+}
